0109bc22244178ad34c8b970c27f6ce34b88c7a8
Marin Godechot <marin.godechot@gmail.com>
2019-12-07 22:22:28 +0100

    working basic table view

--- front-end/src/components/pages/TableView.tsx
+++ front-end/src/components/pages/TableView.tsx
@@ -1,39 +1,58 @@
 import * as React from 'react';
 import { Table } from 'react-bulma-components';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { sentenceCase } from 'sentence-case';
+
+type Event = {
+  id: string;
+  caregiver_id: string;
+  care_recipient_id: string;
+  timestamp: string;
+  event_type: string;
+};
 
 function TableView() {
+  const [events, setEvents] = useState([] as Array<Event>);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(
+    () => {
+      const fetchEvents = async () => {
+        setIsLoading(true);
+        const result = await axios('/events');
+        setEvents(result.data.results);
+        setIsLoading(false);
+      };
+      fetchEvents();
+    },
+    []);
   return (
-    <Table>
-      <thead>
-      <tr>
-        <th>Care recipient</th>
-        <th>Event type</th>
-        <th>Time</th>
-        <th>Caregiver</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <th>Machin</th>
-        <td>Visit cancelled</td>
-        <td>18:00 04-12-19</td>
-        <th>Truc</th>
-      </tr>
-      <tr>
-        <th>Bidule</th>
-        <td>Concern raised</td>
-        <td>19:00 04-12-19</td>
-        <th>Truc</th>
-      </tr>
-      <tr>
-        <th>Machin</th>
-        <td>Mental health observation</td>
-        <td>19:30 04-12-19</td>
-        <th>Truc</th>
-      </tr>
-      </tbody>
-    </Table>
+    isLoading ? (
+      <div>Loading...</div>
+    ) : (
+      <Table>
+        <thead>
+        <tr>
+          <th>Care recipient</th>
+          <th>Event type</th>
+          <th>Time</th>
+          <th>Caregiver</th>
+        </tr>
+        </thead>
+        <tbody>
+        {events.map(event => (
+          <tr key={event.id}>
+            <th>{event.care_recipient_id}</th>
+            <td>{sentenceCase(event.event_type)}</td>
+            <td>{event.timestamp}</td>
+            <th>{event.caregiver_id}</th>
+          </tr>
+        ))}
+        </tbody>
+      </Table>
+    )
   );
 }
 
